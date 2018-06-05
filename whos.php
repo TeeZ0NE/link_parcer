14fbca3997520b3ff66cef33d137c1170eb989ea
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-06-05 17:47:19 +0300

    s

--- whos.php
+++ whos.php
@@ -37,7 +37,7 @@ function get_data($result, $query, $output_file, $log)
 			if (!empty($op_cr_arr)) $created = $op_cr_arr[1];
 			# expires
 			preg_match("/exp[\w]*:\s*([-\w\d\.]+)/i", $value, $op_exp_arr);
-			if (!empty($op_cr_arr)) $expires = $op_exp_arr[1];
+			if (!empty($op_exp_arr)) $expires = $op_exp_arr[1];
 			# ip adresses
 			preg_match("/ip[-\w]*:\s*([-\w\d\.]+)/i", $value, $op_ip_arr);
 			if (!empty($op_ip_arr)) array_push($ip_arr, $op_ip_arr[1]);
