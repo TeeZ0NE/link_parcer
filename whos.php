9dc9ed3dce4bcafcfcb3f5cc4fc938a82359102a
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-06-05 15:21:59 +0300

    try another

--- whos.php
+++ whos.php
@@ -11,8 +11,8 @@ $result = $whois->lookup($query, true);
 echo "<pre>";
 print_r($result);
 echo "</pre>";
-if(in_array("Nserver",$result["rawdata"])) $result["rawdata"]; else echo"dont";
-print_r($result["rawdata"]);
+if(in_array("Nserver:",$result["rawdata"])) $result["rawdata"]; else echo"dont";
+print_r($result["reginfo"]["registered"]);
 /*
 # Reading file line by line
 $file = fopen ("download/urls_list.txt","r");
