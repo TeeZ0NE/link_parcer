96a2133df2917c3b3b992fdd8d7fe1f4fdfd73ec
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-06-05 12:06:37 +0300

    unncommited

--- whos.php
+++ whos.php
@@ -5,14 +5,14 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 	require(__DIR__ . '/vendor/autoload.php');
 }
 $query = 'example.com';
-/*$whois = new Whois();
+$whois = new Whois();
 $result = $whois->lookup($query, false);
 echo "<pre>";
 print_r($result);
-echo "</pre>";*/
+echo "</pre>";
 
 # Reading file line by line
-$file = fopen ("download/urls_list.txt","r");
+/*$file = fopen ("download/urls_list.txt","r");
 $output_file = "download/test.md";
 header('Content-Type: text/plain');
 if(!file_exists($output_file)){
@@ -23,14 +23,14 @@ if(!file_exists($output_file)){
 while(!feof($file)) {
 	$testing_url = fgets($file);
 //	echo "testing string: " . $testing_url;
-	preg_match("/.*/",$testing_url,$output);
+	preg_match("/.",$testing_url,$output);
 	$string = "site|$output[0]\n";
 	file_put_contents($output_file,$string,FILE_APPEND | LOCK_EX);
 }
 echo "well done";
 
 # sorting and writing file
-/*
+
 $contents = file_get_contents("download/urls.txt");
 //print_r($contents);
 $result = preg_replace("/(https?:\/\/[-\d\w\.]+)\/?(.*)/", "$1", $contents);
