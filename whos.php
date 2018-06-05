ac43abf5684f05dcb4ba2aee15100bdc9f20c575
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-06-05 12:25:58 +0300

    add testing url

--- whos.php
+++ whos.php
@@ -4,7 +4,7 @@ ini_set("display_errors", 1);
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 	require(__DIR__ . '/vendor/autoload.php');
 }
-$query = 'example.com';
+$query = 'http://yugstal.com';
 $whois = new Whois();
 $result = $whois->lookup($query, false);
 echo "<pre>";
